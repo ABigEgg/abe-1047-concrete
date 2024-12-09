0ea2d1610697f4dfc9ec679061d939c790fb6f25
David Hewitson <david@abigegg.com>
2024-12-09 15:56:47 +0000

    WIP

---
+++ wp-content/themes/concrete/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "views/*.twig" ],
+  theme: {
+    colors: {
+      "cc-mid-grey": "#888c8c",
+      "cc-dark-grey": "#464d53",
+      "cc-light-grey": "#d9d9d9",
+      "cc-grey": "#d9d9d9"
+    }
+  },
+  plugins: [],
+}
+
