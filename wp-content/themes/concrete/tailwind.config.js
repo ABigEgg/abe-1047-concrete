9f96313a4da0d2efea9bdfd6735f0e9341679786
David Hewitson <david@abigegg.com>
2025-01-20 17:18:37 +0000

    WIP

--- wp-content/themes/concrete/tailwind.config.js
+++ wp-content/themes/concrete/tailwind.config.js
@@ -1,4 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
+function rem(px) {
+  return ( px / 16 ).toFixed(2) + "rem";
+}
+
 module.exports = {
   content: [ "views/*.twig" ],
   theme: {
@@ -6,7 +11,25 @@ module.exports = {
       "cc-mid-grey": "#888c8c",
       "cc-dark-grey": "#464d53",
       "cc-light-grey": "#d9d9d9",
-      "cc-grey": "#d9d9d9"
+      "cc-grey": "#d9d9d9",
+      "cc-white": "#fff",
+    },
+    fontSize: {
+      "2xl": [ rem(105), rem(105) ],
+      "xl": [ rem(42), rem(50) ],
+      "lg": rem(36),
+      "2md": [ rem(32), rem(38.4) ],
+      "md": rem(30),
+      "sm": rem(24),
+      "xs": rem(18),
+    },
+    container: {
+      "center": true,
+    },
+    extend: {
+      spacing: {
+        'container': '70rem'
+      }
     }
   },
   plugins: [],
