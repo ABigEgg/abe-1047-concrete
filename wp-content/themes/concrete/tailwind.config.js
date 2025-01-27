3444c7554d1d9f2cbc08a4714a425b9378274108
David Hewitson <david@abigegg.com>
2025-01-27 15:51:29 +0000

    WIP

--- wp-content/themes/concrete/tailwind.config.js
+++ wp-content/themes/concrete/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       "xl": [ rem(42), rem(50) ],
       "lg": rem(36),
       "2md": [ rem(32), rem(38.4) ],
-      "md": rem(30),
+      "md": [ rem(30), rem(36) ],
       "sm": [ rem(24), rem(28.8) ],
       "xs": rem(18),
     },
