09a1f6157fbced74b65ba54e2ce24e21f48ba6f7
David Hewitson <david@abigegg.com>
2025-03-20 16:26:45 +0000

    WIP

--- wp-content/themes/concrete/inc/acf/options.php
+++ wp-content/themes/concrete/inc/acf/options.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ACF Field Groups for Site Options
  * 
@@ -6,129 +7,196 @@
  * Location: ACF Options Page
  */
 
-if( function_exists('acf_add_local_field_group') ):
+if (function_exists('acf_add_local_field_group')):
 
-// Our Services Section (Global)
-acf_add_local_field_group(array(
-    'key' => 'group_services_settings',
-    'title' => 'Our Services Section',
-    'fields' => array(
-        array(
-            'key' => 'field_services_heading',
-            'label' => 'Section Heading',
-            'name' => 'services_heading',
-            'type' => 'text',
-            'instructions' => 'Enter the heading for the Our Services section',
-            'required' => 1,
-            'default_value' => 'Our services',
-        ),
-        array(
-            'key' => 'field_services_description',
-            'label' => 'Section Description',
-            'name' => 'services_description',
-            'type' => 'textarea',
-            'instructions' => 'Enter the description text for the Our Services section',
-            'required' => 1,
-            'default_value' => 'Your brand needs to be as versatile as your business. It has to be bold, visible, clever, dynamic, inspire and move. That is why we have a range of services to facilitate all of your needs.',
-            'rows' => 4,
-        ),
-        array(
-            'key' => 'field_services_items',
-            'label' => 'Service Items',
-            'name' => 'services_items',
-            'type' => 'relationship',
-            'instructions' => 'Select up to 5 services to display in the Our Services section',
-            'required' => 1,
-            'post_type' => array(
-                0 => 'service',
-            ),
-            'filters' => array(
-                0 => 'search',
-            ),
-            'min' => 1,
-            'max' => 5,
-            'return_format' => 'object',
-        ),
-    ),
-    'location' => array(
-        array(
+    // Our Services Section (Global)
+    acf_add_local_field_group(array(
+        'key' => 'group_services_settings',
+        'title' => 'Our Services Section',
+        'fields' => array(
             array(
-                'param' => 'options_page',
-                'operator' => '==',
-                'value' => 'site-options',
+                'key' => 'field_services_heading',
+                'label' => 'Section Heading',
+                'name' => 'services_heading',
+                'type' => 'text',
+                'instructions' => 'Enter the heading for the Our Services section',
+                'required' => 1,
+                'default_value' => 'Our services',
             ),
-        ),
-    ),
-    'menu_order' => 0,
-    'position' => 'normal',
-    'style' => 'default',
-    'label_placement' => 'top',
-    'instruction_placement' => 'label',
-    'hide_on_screen' => '',
-    'active' => true,
-    'description' => 'Fields for the Our Services section that appears globally throughout the site',
-));
-
-// Global CTA Section
-acf_add_local_field_group(array(
-    'key' => 'group_global_cta',
-    'title' => 'Call To Action Section',
-    'fields' => array(
-        array(
-            'key' => 'field_global_cta_heading',
-            'label' => 'CTA Heading',
-            'name' => 'global_cta_heading',
-            'type' => 'text',
-            'instructions' => 'Enter the heading for the call to action section',
-            'required' => 1,
-            'default_value' => 'Have an idea of a project you\'d like to work on?',
-        ),
-        array(
-            'key' => 'field_global_cta_text',
-            'label' => 'CTA Text',
-            'name' => 'global_cta_text',
-            'type' => 'textarea',
-            'instructions' => 'Enter the descriptive text for the call to action',
-            'required' => 1,
-            'default_value' => 'For more information on this project, please contact us.',
-            'rows' => 3,
-        ),
-        array(
-            'key' => 'field_global_cta_button_text',
-            'label' => 'Button Text',
-            'name' => 'global_cta_button_text',
-            'type' => 'text',
-            'instructions' => 'Enter the text for the CTA button',
-            'required' => 1,
-            'default_value' => 'Contact Us',
-        ),
-        array(
-            'key' => 'field_global_cta_button_url',
-            'label' => 'Button URL',
-            'name' => 'global_cta_button_url',
-            'type' => 'url',
-            'instructions' => 'Enter the URL for the CTA button',
-            'required' => 1,
-            'default_value' => '/contact',
-        ),
-    ),
-    'location' => array(
-        array(
             array(
-                'param' => 'options_page',
-                'operator' => '==',
-                'value' => 'site-options',
+                'key' => 'field_services_description',
+                'label' => 'Section Description',
+                'name' => 'services_description',
+                'type' => 'textarea',
+                'instructions' => 'Enter the description text for the Our Services section',
+                'required' => 1,
+                'default_value' => 'Your brand needs to be as versatile as your business. It has to be bold, visible, clever, dynamic, inspire and move. That is why we have a range of services to facilitate all of your needs.',
+                'rows' => 4,
+            ),
+            array(
+                'key' => 'field_services_items',
+                'label' => 'Service Items',
+                'name' => 'services_items',
+                'type' => 'relationship',
+                'instructions' => 'Select up to 5 services to display in the Our Services section',
+                'required' => 1,
+                'post_type' => array(
+                    0 => 'service',
+                ),
+                'filters' => array(
+                    0 => 'search',
+                ),
+                'min' => 1,
+                'max' => 5,
+                'return_format' => 'object',
             ),
         ),
-    ),
-    'menu_order' => 10,
-    'position' => 'normal',
-    'style' => 'default',
-    'label_placement' => 'top',
-    'instruction_placement' => 'label',
-    'hide_on_screen' => '',
-    'active' => true,
-    'description' => 'Fields for the CTA section that appears globally throughout the site',
-));
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'site-options',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => 'Fields for the Our Services section that appears globally throughout the site',
+    ));
 
-endif; 
+    // Global CTA Section
+    acf_add_local_field_group(array(
+        'key' => 'group_global_cta',
+        'title' => 'Call To Action Section',
+        'fields' => array(
+            array(
+                'key' => 'field_global_cta_heading',
+                'label' => 'CTA Heading',
+                'name' => 'global_cta_heading',
+                'type' => 'text',
+                'instructions' => 'Enter the heading for the call to action section',
+                'required' => 1,
+                'default_value' => 'Have an idea of a project you\'d like to work on?',
+            ),
+            array(
+                'key' => 'field_global_cta_text',
+                'label' => 'CTA Text',
+                'name' => 'global_cta_text',
+                'type' => 'textarea',
+                'instructions' => 'Enter the descriptive text for the call to action',
+                'required' => 1,
+                'default_value' => 'For more information on this project, please contact us.',
+                'rows' => 3,
+            ),
+            array(
+                'key' => 'field_global_cta_button_text',
+                'label' => 'Button Text',
+                'name' => 'global_cta_button_text',
+                'type' => 'text',
+                'instructions' => 'Enter the text for the CTA button',
+                'required' => 1,
+                'default_value' => 'Contact Us',
+            ),
+            array(
+                'key' => 'field_global_cta_button_url',
+                'label' => 'Button URL',
+                'name' => 'global_cta_button_url',
+                'type' => 'url',
+                'instructions' => 'Enter the URL for the CTA button',
+                'required' => 1,
+                'default_value' => '/contact',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'site-options',
+                ),
+            ),
+        ),
+        'menu_order' => 10,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => 'Fields for the CTA section that appears globally throughout the site',
+    ));
+
+    // Contact Information Section
+    acf_add_local_field_group(array(
+        'key' => 'group_contact_info',
+        'title' => 'Contact Information',
+        'fields' => array(
+            array(
+                'key' => 'field_contact_email',
+                'label' => 'Email Address',
+                'name' => 'contact_email',
+                'type' => 'email',
+                'instructions' => 'Enter the primary contact email address',
+                'required' => 1,
+                'default_value' => 'hello@concretecreative.co.uk',
+            ),
+            array(
+                'key' => 'field_contact_phone',
+                'label' => 'Phone Number',
+                'name' => 'contact_phone',
+                'type' => 'text',
+                'instructions' => 'Enter the primary contact phone number',
+                'required' => 1,
+                'default_value' => '0141 724 0089',
+            ),
+            array(
+                'key' => 'field_contact_twitter',
+                'label' => 'Twitter URL',
+                'name' => 'contact_twitter',
+                'type' => 'url',
+                'instructions' => 'Enter the Twitter profile URL',
+                'required' => 0,
+            ),
+            array(
+                'key' => 'field_contact_instagram',
+                'label' => 'Instagram URL',
+                'name' => 'contact_instagram',
+                'type' => 'url',
+                'instructions' => 'Enter the Instagram profile URL',
+                'required' => 0,
+            ),
+            array(
+                'key' => 'field_contact_linkedin',
+                'label' => 'LinkedIn URL',
+                'name' => 'contact_linkedin',
+                'type' => 'url',
+                'instructions' => 'Enter the LinkedIn profile URL',
+                'required' => 0,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'site-options',
+                ),
+            ),
+        ),
+        'menu_order' => 20,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => 'Contact information fields for the footer and other sections of the site',
+    ));
+
+endif;
--- wp-content/themes/concrete/inc/theme.inc.php
+++ wp-content/themes/concrete/inc/theme.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Theme setup and asset enqueuing
  */
@@ -8,7 +9,8 @@ if (!defined('ABSPATH')) exit;
 /**
  * Enqueue theme scripts and styles
  */
-function concrete_enqueue_assets() {
+function concrete_enqueue_assets()
+{
     $theme_version = wp_get_theme()->get('Version');
     $asset_version = $theme_version . '.' . filemtime(get_template_directory() . '/dist/css/main.css');
 
@@ -41,7 +43,8 @@ add_action('wp_enqueue_scripts', 'concrete_enqueue_assets');
 /**
  * Add custom theme supports
  */
-function concrete_theme_support() {
+function concrete_theme_support()
+{
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('html5', [
@@ -55,7 +58,7 @@ function concrete_theme_support() {
     ]);
 
     add_theme_support('align-wide');
-    
+
     // Register navigation menu locations
     register_nav_menus([
         'main-navigation' => __('Main Navigation', 'concrete'),
@@ -65,37 +68,42 @@ function concrete_theme_support() {
 add_action('after_setup_theme', 'concrete_theme_support');
 
 // Register custom image sizes
-add_action('after_setup_theme', function() {
+add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
-    
+
     // Project card thumbnail size
     add_image_size('project-card', 544, 376, true); // Width, Height, Crop
-    
+
     // Double height project card size
     add_image_size('project-card-double', 571, 823, true); // Width, Height, Crop for double height cards
-    
+
     // Thumbnail size for project cards
     add_image_size('project-thumbnail', 150, 150, true); // Small thumbnail for admin or listing views
 });
 
 // Add menus to Timber context
 add_filter('timber/context', 'concrete_add_to_context');
-function concrete_add_to_context($context) {
+function concrete_add_to_context($context)
+{
     // Add main navigation to context
     $context['main_menu'] = new Timber\Menu('main-navigation');
-    
+
     // Add mobile navigation to context
     $context['mobile_menu'] = new Timber\Menu('mobile-navigation');
-    
+
+    // Add ACF options to context
+    $context['options'] = get_fields('option');
+
     return $context;
-} 
+}
 
 /**
  * Enable SVG upload support
  * 
  * Add SVG to allowed mime types and fix display in media library
  */
-function concrete_enable_svg_uploads($mimes) {
+function concrete_enable_svg_uploads($mimes)
+{
     $mimes['svg'] = 'image/svg+xml';
     $mimes['svgz'] = 'image/svg+xml';
     return $mimes;
@@ -105,7 +113,8 @@ add_filter('upload_mimes', 'concrete_enable_svg_uploads');
 /**
  * Fix SVG display in media library
  */
-function concrete_fix_svg_media_display() {
+function concrete_fix_svg_media_display()
+{
     echo '<style type="text/css">
         .attachment-266x266, .thumbnail img {
             width: 100% !important;
@@ -118,16 +127,17 @@ add_action('admin_head', 'concrete_fix_svg_media_display');
 /**
  * Fix SVG size attributes for proper display
  */
-function concrete_fix_svg_size_attributes($response, $attachment, $meta) {
+function concrete_fix_svg_size_attributes($response, $attachment, $meta)
+{
     if ($response['mime'] === 'image/svg+xml' && empty($response['sizes'])) {
         $svg_path = get_attached_file($attachment->ID);
-        
+
         if (!$svg_path) {
             return $response;
         }
-        
+
         $dimensions = concrete_get_svg_dimensions($svg_path);
-        
+
         if ($dimensions) {
             $response['sizes'] = array(
                 'full' => array(
@@ -139,7 +149,7 @@ function concrete_fix_svg_size_attributes($response, $attachment, $meta) {
             );
         }
     }
-    
+
     return $response;
 }
 add_filter('wp_prepare_attachment_for_js', 'concrete_fix_svg_size_attributes', 10, 3);
@@ -147,17 +157,18 @@ add_filter('wp_prepare_attachment_for_js', 'concrete_fix_svg_size_attributes', 1
 /**
  * Get SVG dimensions helper function
  */
-function concrete_get_svg_dimensions($svg_path) {
+function concrete_get_svg_dimensions($svg_path)
+{
     $svg = simplexml_load_file($svg_path);
-    
+
     if ($svg === false) {
         return false;
     }
-    
+
     $attributes = $svg->attributes();
     $width = (string) $attributes->width;
     $height = (string) $attributes->height;
-    
+
     // Check if width and height are defined
     if (!$width || !$height) {
         // Check for viewBox attribute
@@ -170,13 +181,13 @@ function concrete_get_svg_dimensions($svg_path) {
             }
         }
     }
-    
+
     if ($width && $height) {
         return array(
             'width' => intval($width),
             'height' => intval($height)
         );
     }
-    
+
     return false;
-} 
+}
