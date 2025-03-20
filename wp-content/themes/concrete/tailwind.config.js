2fa9aa2c4388b3065f725da4277a73723b262778
David Hewitson <david@abigegg.com>
2025-03-20 16:16:32 +0000

    WIP

--- wp-content/themes/concrete/tailwind.config.js
+++ wp-content/themes/concrete/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 function rem(px) {
-  return ( px / 16 ).toFixed(2) + "rem";
+  return (px / 16).toFixed(2) + "rem";
 }
 
 module.exports = {
-  content: [ 
+  content: [
     "views/**/*.twig",
-    "views/*.twig" 
+    "views/*.twig"
   ],
   theme: {
     screens: {
@@ -25,19 +25,23 @@ module.exports = {
       "cc-white": "#fff",
     },
     fontSize: {
-      "3xl": [ rem(105), rem(105) ],
-      "2xl": [ rem(52), rem(62.4) ],
-      "xl": [ rem(42), rem(50) ],
+      "3xl": [rem(105), rem(105)],
+      "2xl": [rem(52), rem(62.4)],
+      "xl": [rem(42), rem(50)],
       "lg": rem(36),
-      "2md": [ rem(32), rem(38.4) ],
-      "md": [ rem(30), rem(36) ],
-      "sm": [ rem(24), rem(28.8) ],
+      "2md": [rem(32), rem(38.4)],
+      "md": [rem(30), rem(36)],
+      "sm": [rem(24), rem(28.8)],
       "xs": rem(18),
+      "btn": [rem(22), rem(20)],
+      "footer": [rem(18), rem(21)],
+      "tag": [rem(10.75), rem(23)],
+      "copyright": [rem(12), rem(14.4)],
     },
     container: {
       "center": true,
       padding: {
-        DEFAULT: '1rem',
+        DEFAULT: '2rem',
         sm: '1.5rem',
         lg: '2rem'
       },
